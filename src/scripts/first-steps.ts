44c515e02b2b0bc69c3840b069dee0ca14878c51
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2022-03-08 22:00:43 -0500

    * Setup iterative vs recursive

--- src/scripts/first-steps.ts
+++ src/scripts/first-steps.ts
@@ -1,1 +1,26 @@
-console.log('test');
+const numberSet: Array<number> = [2, 4, 6, 8, 10];
+
+export const iterativeSum = (numbers: Array<number>): number => {
+  let total = 0;
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    total += numbers[i];
+  }
+
+  return total;
+};
+
+export const recursiveSum = (numbers: Array<number>): number => {
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  if (numbers.length === 1) {
+    return numbers[0];
+  }
+
+  return numbers[0] + recursiveSum(numbers.slice(1));
+};
+
+console.log(iterativeSum(numberSet));
+console.log(recursiveSum(numberSet));
