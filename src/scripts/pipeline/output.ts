94cd5b0c99eeec16adb396513aab805a847f61b0
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2022-03-19 22:49:45 -0400

    * Added new functions for the pipeline set and created a pipeline function that uses the reduce function.

--- src/scripts/pipeline/output.ts
+++ src/scripts/pipeline/output.ts
@@ -1,6 +1,13 @@
-import { loveSomeShow, showSomeLove } from './utilities';
+import {
+  composedPipe,
+  composedPipe2,
+  loveSomeShow,
+  showSomeLove,
+} from './utilities';
 
 console.log(showSomeLove('pipeline'));
 console.log(showSomeLove('pure function'));
 console.log(showSomeLove('coffee break'));
 console.log(loveSomeShow('pipeline'));
+console.log(composedPipe('composition'));
+console.log(composedPipe2('composition'));
--- src/scripts/pipeline/utilities.ts
+++ src/scripts/pipeline/utilities.ts
@@ -1,5 +1,6 @@
 import { head } from '../arrays/head';
 import { length } from '../arrays/length';
+import { reduce } from '../arrays/reduce';
 import { tail } from '../arrays/tail';
 
 export const pipeline =
@@ -18,6 +19,15 @@ export const pipeline =
     return pipeline(...tail(functionList))(transformedInput);
   };
 
+export const pipeline2 =
+  (...functionList: Array<Function>) =>
+  <T>(pipelineInput: T): T =>
+    reduce(
+      (val: string, currentFunc: Function) => currentFunc(val),
+      pipelineInput,
+      functionList
+    );
+
 export const pluralize = (singularWord: string) => `${singularWord}s`;
 
 export const heart = (word: string) => `I ❤️ ${word}`;
@@ -25,7 +35,13 @@ export const heart = (word: string) => `I ❤️ ${word}`;
 export const exclaim = (sentence: string) => `${sentence}!`;
 
 export const showSomeLove = (input: string) =>
-  pipeline(pluralize, heart, exclaim)(input);
+  pipeline2(pluralize, heart, exclaim)(input);
 
 export const loveSomeShow = (input: string) =>
-  pipeline(exclaim, heart, pluralize)(input);
+  pipeline2(exclaim, heart, pluralize)(input);
+
+export const composedPipe = (input: string) =>
+  pipeline2(exclaim)(pipeline2(heart, pluralize)(input));
+
+export const composedPipe2 = (input: string) =>
+  pipeline2(pipeline2(pluralize, heart), exclaim)(input);
