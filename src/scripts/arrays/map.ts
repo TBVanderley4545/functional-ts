bb845bd81c4c57f7b79c7208004d727ce8b24804
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2022-03-20 00:25:54 -0400

    * Change typer parameter position.

--- src/scripts/arrays/map.ts
+++ src/scripts/arrays/map.ts
@@ -4,8 +4,8 @@ import { length } from './length';
 import { tail } from './tail';
 
 export const map =
-  (predicateFn: Function, index: number = 0) =>
-  <T>(inputArray: Array<T>): Array<T> => {
+  <T>(predicateFn: Function, index: number = 0) =>
+  (inputArray: Array<T>): Array<T> => {
     if (length(inputArray) === 0) {
       return [];
     }
